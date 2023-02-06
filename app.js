3f658793ad49346de7f0d5969067f50616fed44d
sheng yeong <chia.shengyeong@gmail.com>
2023-02-06 16:15:44 +0800

    remove non used code

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
